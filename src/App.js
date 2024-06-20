5c6499ce4139259cc43cd71ab1c939e39b9bd156
jeinieldjs <silvestrejeiniel@gmail.com>
2024-06-20 14:12:56 +0800

    added addtask component, displayed data on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 import './App.css';
+import data from './assets/tasks.json';
 
 function App() {
   return (
     <div className="App">
      <h1>Things To Do</h1>
+     {
+      data.map((task) => {
+        return <div>
+          <ul>
+            <li key={task.id}>{task.task}</li>
+          </ul>
+        </div>
+      })
+     }
     </div>
   );
 }
